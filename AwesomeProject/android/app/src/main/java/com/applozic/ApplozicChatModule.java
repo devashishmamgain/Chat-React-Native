d4169499f29bcd64e2e3b621a1e080afc95c64ba
akshatagg <akshatagg07@gmail.com>
2017-01-03 06:14:33 -0800

    Made change in 2 files & App is working

--- AwesomeProject/android/app/src/main/java/com/applozic/ApplozicChatModule.java
+++ AwesomeProject/android/app/src/main/java/com/applozic/ApplozicChatModule.java
@@ -1,16 +1,9 @@
-
 package com.applozic;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-
-
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 
 import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
 import com.applozic.mobicomkit.api.account.user.MobiComUserPreference;
@@ -19,6 +12,7 @@ import com.applozic.mobicomkit.api.account.user.UserClientService;
 import com.applozic.mobicomkit.api.account.user.UserLoginTask;
 import com.applozic.mobicomkit.uiwidgets.conversation.activity.ConversationActivity;
 import com.applozic.mobicomkit.uiwidgets.conversation.ConversationUIService;
+import com.applozic.mobicommons.people.channel.Conversation;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -26,48 +20,39 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.applozic.mobicomkit.api.conversation.database.MessageDatabaseService;
+import com.applozic.mobicomkit.feed.TopicDetail;
 
-
-public class ApplozicChatModule extends ReactContextBaseJavaModule implements ActivityEventListener{
-
+public class ApplozicChatModule extends ReactContextBaseJavaModule implements  ActivityEventListener {
 
     public ApplozicChatModule(ReactApplicationContext reactContext) {
-         super(reactContext);
-         reactContext.addActivityEventListener(this);
-     }
-
-     
-    
-
+    super(reactContext);
+    reactContext.addActivityEventListener(this);
+    }
 
     @Override
     public String getName() {
-        return "ApplozicChat";
+    return "ApplozicChat";
     }
 
     @ReactMethod
     public void login(ReadableMap config, final Callback successCallback,final Callback cancelCallback) {
-      Activity currentActivity = getCurrentActivity();
+    Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+     cancelCallback.invoke("Activity doesn't exist");
+     return;}
+	 
+    UserLoginTask.TaskListener listener = new UserLoginTask.TaskListener()
+	{      
+	@Override
+    public void onSuccess(RegistrationResponse registrationResponse, Context context) {
+    //After successful registration with Applozic server the callback will come here
+    successCallback.invoke("success");}
 
-      if (currentActivity == null) {
-          cancelCallback.invoke("Activity doesn't exist");
-          return;
-      }
-
-        UserLoginTask.TaskListener listener = new UserLoginTask.TaskListener() {
-
-            @Override
-            public void onSuccess(RegistrationResponse registrationResponse, Context context) {
-                //After successful registration with Applozic server the callback will come here
-                successCallback.invoke("success");
-            }
-
-            @Override
-            public void onFailure(RegistrationResponse registrationResponse, Exception exception) {
-                //If any failure in registration the callback  will come here
-				cancelCallback.invoke("failure");
-			
-			}};
+    @Override
+    public void onFailure(RegistrationResponse registrationResponse, Exception exception) {
+    //If any failure in registration the callback  will come here
+    cancelCallback.invoke("failure");}
+	 };
 
         User user = new User();
         user.setUserId(config.getString("userId")); //userId it can be any unique user identifier
@@ -77,118 +62,95 @@ public class ApplozicChatModule extends ReactContextBaseJavaModule implements Ac
         user.setPassword(config.getString("password")); //optional, leave it blank for testing purpose, read this if you want to add additional security by verifying password from your server https://www.applozic.com/docs/configuration.html#access-token-url
         user.setImageLink("");//optional,pass your image link
         user.setApplicationId("applozic-sample-app");
-        new UserLoginTask(user, listener, currentActivity).execute((Void) null);
-        
+        new UserLoginTask(user, listener, currentActivity).execute((Void) null);        
 	}
 
     @ReactMethod
     public void openChat(ReadableMap config, final Callback successCallback,final Callback cancelCallback) {
-        Activity currentActivity = getCurrentActivity();
-
-        if (currentActivity == null) {
-            cancelCallback.invoke("Activity doesn't exist");
-            return;
-        }
-
-        Intent intent = new Intent(currentActivity, ConversationActivity.class);
-        currentActivity.startActivity(intent);
+    Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+    cancelCallback.invoke("Activity doesn't exist");
+    return;}
+	Intent intent = new Intent(currentActivity, ConversationActivity.class);
+    currentActivity.startActivity(intent);
     }
 
     @ReactMethod
-    public void initiateChat(ReadableMap config, final Callback successCallback,final Callback cancelCallback)
-    {
-        Activity currentActivity = getCurrentActivity();
-
-        if (currentActivity == null) {
-            cancelCallback.invoke("Activity doesn't exist");
-            return;
-        }
-
-        Intent intent = new Intent(currentActivity, ConversationActivity.class);
-        if(config!=null && config.hasKey("userId")) {
-            intent.putExtra(ConversationUIService.USER_ID, config.getString("userId"));
-        }
-        if(config!=null && config.hasKey("displayName")) {
-            intent.putExtra(ConversationUIService.DISPLAY_NAME, config.getString("displayName")); //put it for displaying the title.
-        }
-        currentActivity.startActivity(intent);
+    public void initiateChat(ReadableMap config, final Callback successCallback,final Callback cancelCallback){
+    Activity currentActivity = getCurrentActivity();
+     if (currentActivity == null) {
+     cancelCallback.invoke("Activity doesn't exist");
+     return;}
+     Intent intent = new Intent(currentActivity, ConversationActivity.class);
+     if(config!=null && config.hasKey("userId")) {
+     intent.putExtra(ConversationUIService.USER_ID, config.getString("userId"));
+     }
+     if(config!=null && config.hasKey("displayName")) {
+     intent.putExtra(ConversationUIService.DISPLAY_NAME, config.getString("displayName")); //put it for displaying the title.
+     }
+      currentActivity.startActivity(intent);
     }
 
     @ReactMethod
     public void logoutUser(ReadableMap config, final Callback successCallback,final Callback cancelCallback)
     {
-        Activity currentActivity = getCurrentActivity();
-
-        if (currentActivity == null) {
-            cancelCallback.invoke("Activity doesn't exist");
-            return;
-        }
-		
-        new UserClientService(currentActivity).logout();
+    Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+    cancelCallback.invoke("Activity doesn't exist");
+    return;
+     }	
+     new UserClientService(currentActivity).logout();
+     successCallback.invoke("success");
     }
 
     @ReactMethod
     public void contactUnreadCount(ReadableMap config, final Callback successCallback,final Callback cancelCallback) {
-        Activity currentActivity = getCurrentActivity();
-
-        if (currentActivity == null) {
-            cancelCallback.invoke("Activity doesn't exist");
-            return;
-        }
-        if(config!=null && config.hasKey("userId")) {
-            int contactUnreadCount = new MessageDatabaseService(currentActivity).getUnreadMessageCountForContact(config.getString("userId"));
-            successCallback.invoke(contactUnreadCount);
-        }
+    Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+    cancelCallback.invoke("Activity doesn't exist");
+    return;
+    }
+    if(config!=null && config.hasKey("userId")) {
+    int contactUnreadCount = new MessageDatabaseService(currentActivity).getUnreadMessageCountForContact(config.getString("userId"));
+    successCallback.invoke(contactUnreadCount);
+     }
     }
 
     @ReactMethod
     public void channelUnreadCount(ReadableMap config, final Callback successCallback,final Callback cancelCallback) {
-        Activity currentActivity = getCurrentActivity();
-
-        if (currentActivity == null) {
-            cancelCallback.invoke("Activity doesn't exist");
-            return;
-        }
-        if(config!=null && config.hasKey("channelKey")) {
-            int channelUnreadCount = new MessageDatabaseService(currentActivity).getUnreadMessageCountForChannel((Integer.parseInt(config.getString("channelKey"))));
-            successCallback.invoke(channelUnreadCount);
-        }
+    Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+     cancelCallback.invoke("Activity doesn't exist");
+     return;
+    }
+    if(config!=null && config.hasKey("channelKey")) {
+    int channelUnreadCount = new MessageDatabaseService(currentActivity).getUnreadMessageCountForChannel((Integer.parseInt(config.getString("channelKey"))));
+    successCallback.invoke(channelUnreadCount);
+     }
     }
 
     @ReactMethod
     public void totalUnreadCount(ReadableMap config, final Callback successCallback,final Callback cancelCallback) {
-        Activity currentActivity = getCurrentActivity();
-
-        if (currentActivity == null) {
-            cancelCallback.invoke("Activity doesn't exist");
-            return;
-        }
-        if(config!=null) {
-            int totalUnreadCount = new MessageDatabaseService(currentActivity).getTotalUnreadCount();
-            successCallback.invoke(totalUnreadCount);
-        }
+    Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+    cancelCallback.invoke("Activity doesn't exist");
+    return;
+    }
+     if(config!=null) {
+     int totalUnreadCount = new MessageDatabaseService(currentActivity).getTotalUnreadCount();
+     successCallback.invoke(totalUnreadCount);
+     }
     }
     
 	@ReactMethod
-	public boolean isUserLogIn(ReadableMap config, final Callback successCallback,final Callback cancelCallback)
+	public void isUserLogIn(ReadableMap config, final Callback successCallback,final Callback cancelCallback)
 	{
-		Activity currentActivity = getCurrentActivity();
-        MobiComUserPreference mobiComUserPreference=MobiComUserPreference.getInstance(currentActivity);
-        //boolean output= 
-	   return	mobiComUserPreference.isLoggedIn();
-	    //successCallback.invoke(""+output);
-	  
-	}
-
-   
-   public void onBackPressed() {
-       
-   }    
-
-    @Override
+	 Activity currentActivity = getCurrentActivity();
+     MobiComUserPreference mobiComUserPreference=MobiComUserPreference.getInstance(currentActivity);
+     successCallback.invoke(mobiComUserPreference.isLoggedIn());	
+	} 
+	
     public void onActivityResult(Activity activity, final int requestCode, final int resultCode, final Intent intent) {}
 
-    @Override
     public void onNewIntent(Intent intent) {}
-
 }
--- AwesomeProject/chat.js
+++ AwesomeProject/chat.js
@@ -1,14 +1,15 @@
 //'use strict';
 import React, { Component } from 'react';
-import BackAndroid from 'react-native';
 import { hardwareBackPress, exitApp } from 'react-native-back-android';
+import { BackAndroid } from 'react-native';
 import {
   AppRegistry,
   StyleSheet,
+  AppState,
   Text,
   View,
   Button,
-   TextInput,
+  TextInput,
   ScrollView,
   NativeModules
 } from 'react-native';
@@ -17,94 +18,95 @@ import {
 var ImagePicker  = NativeModules.ImagePicker;
 var ApplozicChat = NativeModules.ApplozicChat;
 
+export default class AwesomeProject extends React.Component{
 
-export default class AwesomeProject extends Component {
-
-  
-	  constructor(props) {
+	constructor(props) {
     super(props);
     this.state = 
 		{userId:'',
 		 email :'',
 		 phoneNumer:'',
-		 password:'',
-		 displayName:'',
-		 blank:''
-	     };
-
+		 pass_word:'',
+		 displayName:'',		 
+         result:false };
 	this.openChat = this.openChat.bind(this);
     this.isUserLogIn=this.isUserLogIn.bind(this);
-
+    this.logoutUser=this.logoutUser.bind(this);
+	this.updateState=this.updateState.bind(this);
+	this.initiateChat=this.initiateChat.bind(this);
+	this.chatLogin=this.chatLogin.bind(this);
+ 	this.onClick=this.onClick.bind(this);
   }
   
+componentDidMount() {
+    this.isUserLogIn;
+  }
   
-  
-  
-  openImage() {
+openImage() {
     ImagePicker.openSelectDialog(
-      {}, // no config yet
-      (uri) => { console.log(uri) },
-      (error) => { console.log(error) }
-    )
-  }
-
-  
- openChat() {
-    ApplozicChat.openChat(
-      {},
-      (response) => { console.log(response) },
-      (error) => { console.log(error) }
-    )
-  }
-
-
- chatLogin() {
-	 var a=this.state.userId;
-	 var b=this.state.password;
-	 var c=this.state.displayName;
-	if(a.length>0 && b.length>0 && c.length>0)
-	 {
-		
-    ApplozicChat.login(
-     {'userId' : this.state.userId, 'email' : '','phoneNumber': '','password' : this.state.password,'displayName' : ''},
-	 (response) => { window.alert('success'),this.openChat()},
-      (error) => { window.alert(error) }
-    )
-	 }
-  }
- 
-
- initiateChat(){
-    ApplozicChat.initiateChat(
-    {},
-    (response) => { console.log(response) },
-    (error) => { console.log(error) }
+    {}, // no config yet
+    (uri) => { console.log(uri) },
+    (error) => { console.log(error) },
    )
  }
 
- logoutUser(){
-
-    ApplozicChat.logoutUser(
-		
-     {},
-     (response) => { this.setState({userId:'',email:'',phoneNumber:'',password:'',displayName:''}),alert("logout successful")},
-     (error) => { window.alert(error)}
+chatLogin() {
+	if(this.state.userId.length>0 && this.state.pass_word.length>0)
+	{
+    ApplozicChat.login(
+    {'userId':this.state.userId,'email':'','phoneNumber':'','password':this.state.pass_word,'displayName':''},
+	(response) => { this.openChat(),this.setState({ result: true })},
+    (error) => {console.log(error)},
     )
+    }else{alert("Please Enter UserId & Password")};
+  }
+ 
+isUserLogIn(){
+    ApplozicChat.isUserLogIn(
+    {},
+    (response) => {alert(response),this.setState({ result: response });},
+    (error) => { console.log(error) },
+   )
+  }
+ 
+logoutUser(){
+    ApplozicChat.logoutUser(
+    {},
+    (response) => {this.setState({userId:'',email:'',phoneNumber:'',pass_word:'',displayName:''})
+	              ,alert("logout successful"),this.setState({result: false})},
+    (error) => {console.log(error)},
+   )
   }
 
- contactUnreadCount(){
+openChat() {
+    ApplozicChat.openChat(
+    {},
+    (response) => {console.log(response)},
+    (error) => {console.log(error)},
+   )
+  }
+  
+initiateChat(){
+    ApplozicChat.initiateChat(
+    {},
+    (response) => {console.log(response)},
+    (error) => {console.log(error)},
+   )
+  }
+
+contactUnreadCount(){
     ApplozicChat.contactUnreadCount(
     {'userId':'ak01'},
-    (response) => { alert(response)  },
-    (error) => { console.log(error) },
+    (response) => {alert(response)},
+    (error) => {console.log(error)},
    )
   }
 
 channelUnreadCount(){
     ApplozicChat.channelUnreadCount(
     {'channelKey':'1234'},
-    (response) => { alert(response) },
-    (error) => { console.log(error) },
+    (response) => {alert(response)},
+    (error) => {console.log(error)},
    )
   }
 
@@ -116,151 +118,125 @@ totalUnreadCount(){
    )
  }
 
- isUserLogIn(){
-      ApplozicChat.isUserLogIn(
-    {},
-    (response) => {console.log(response)},
-    (error) => { console.log(error) },
-   )
-    }
+render() {
+      let display = this.state.result;
+      if (display) {
+        return (    
+        <View style={styles.container}>		
+		<Text style={styles.titleText}>  Applozic </Text>
+        <Text style={styles.baseText} > Demo App   </Text>
+        <Text style={styles.btn} onPress={this.initiateChat}> Chat Logs  </Text>
+		<Text style={styles.btn} onPress={this.logoutUser}> LogOut </Text>	
+        </View>
+	   );}
 
- handleHardwareBackPress() {
-    //console.log('* Scene1 back press');
-	window.alert('* Scene1 back press');
-  //  exitApp();
-    return true;
- }
-//  BackAndroid.addEventListener('hardwareBackPress', function() {
-//
-//           if (!this.isUserLogIn()) 
-//			   {
-//                   this.logoutUser();
-//                   return true;
-//              }
-//                  return false;
-//            });
-
-
-  render() {
-
-    return (
-      <View style={styles.container}>
-          <ScrollView>     
-	<Text style={styles.titleText}>
-          Applozic
-        </Text>
-        <Text style={styles.baseText}>
-          Demo App
-        </Text>
-
-	
-	
-	
-	<TextInput
-		id="userId"
-       style={styles.inputText} 
+       return (
+        <View  style={styles.container}>
+        <ScrollView>
+	    <Text style={styles.titleText}>Applozic </Text>
+        <Text style={styles.baseText}> Demo App </Text>
+	    
+		<TextInput
+        style={styles.inputText} 
 	    keyboardType="default"
-		 placeholder="UserId"
-		  underlineColorAndroid='transparent'
+		placeholder="UserId"
+		maxLength={25}
+		underlineColorAndroid='transparent'
         value={this.state.userId}
-		onChangeText={userId => this.setState({userId})}
+		onChangeText={userId => this.setState({userId})}/>
+        
+		<TextInput
+		type="email-address"
+        style={styles.inputText}
+		placeholder="Email"
+		keyboardType="email-address"
+		maxLength={30}
+		underlineColorAndroid='transparent'
+        value={this.state.email}
+		onChangeText={email => this.setState({email})}/> 
+ 	     
+		<TextInput
+        style={styles.inputText}
+		placeholder="Phone Number"
+		keyboardType="phone-pad"
+		underlineColorAndroid='transparent'
+		maxLength={10}
+        value={this.state.phoneNumber}
+		onChangeText={phoneNumber => this.setState({phoneNumber})}/> 
+          
+		<TextInput
+		id="password"
+		type="password"
+        style={styles.inputText}
+		maxLength={25}
+		placeholder="Password"
+		keyboardType="default"
+		underlineColorAndroid='transparent'
+        value={this.state.pass_word}
+		secureTextEntry={true}
+		onChangeText={pass_word => this.setState({pass_word})}/> 
 
-      />
-         <TextInput
-		  id="email"
+        <TextInput
+	    id="displayName"
         style={styles.inputText}
-		 placeholder="Email"
-		 keyboardType="email-address"
-		  underlineColorAndroid='transparent'
-         value={this.state.email}
-		 onChangeText={email => this.setState({email})}
-      /> 
- 	      <TextInput
-		  id="phoneNo"
-        style={styles.inputText}
-		 placeholder="Phone Number"
-		 keyboardType="phone-pad"
-		 underlineColorAndroid='transparent'
-		  maxLength={10}
-         value={this.state.phoneNumber}
-		 onChangeText={phoneNumber => this.setState({phoneNumber})}
-      /> 
-          <TextInput
-		  id="password"
-        style={styles.inputText}
-		 placeholder="Password"
-		 keyboardType="default"
-		  underlineColorAndroid='transparent'
-         value={this.state.password}
-		 onChangeText={password => this.setState({password})}
-      /> 
-
-         <TextInput
-			 id="displayName"
-        style={styles.inputText}
-		 placeholder="Display Name"
-		 keyboardType="default"
-		  underlineColorAndroid='transparent'
-         value={this.state.displayName}
-		 onChangeText={displayName=> this.setState({displayName})}
-      /> 
-      <Text style={styles.titleText} onPress={this.logoutUser.bind(this)}>
-		  CLICK HERER</Text>
-      <Button
-        onPress={this.chatLogin.bind(this)}
-	 //  onPress={this.onButtonPress.bind(this)}
+		placeholder="Display Name"
+		keyboardType="default"
+		underlineColorAndroid='transparent'
+        value={this.state.displayName}
+		onChangeText={displayName=> this.setState({displayName})}/> 
+      
+	    <Button
+        onPress={this.chatLogin}
         title="Login/SignUp"
 		color ="#20B2AA"
 		marginLeft={20}
-		marginRight={20}
-   			/>
-
- 		 </ScrollView>	  
-
-      </View>
-    );
-  }
+		marginRight={20}/>
+    	
+ 		</ScrollView>	      
+        </View> 
+	  );
+   }
 }
 
 const styles = StyleSheet.create({
-   container: {
+
+container:{
     flex: 1,
     alignItems: 'center',
     backgroundColor: '#4D394B',
   },
-// button: {
-// textAlign: 'center',  
- // height: 100,
- // marginLeft: 10,
- // marginRight: 100,	 
-		
-// },
-  baseText: {
-   fontFamily: 'Cochin',
-	color:'#fff',
-     marginBottom:25,
-   alignSelf:'center',
-      
-	
+  
+btn:{
+	fontSize: 23,
+    fontWeight: 'bold',
+    color:'yellow',
+    marginTop: 20,
+    alignSelf:'center', 
   },
-  titleText: {
+
+baseText:{
+    fontFamily: 'Cochin',
+	color:'#fff',
+    marginBottom:25,
+    alignSelf:'center',
+  },
+  
+titleText:{
     fontSize: 25,
     fontWeight: 'bold',
     color:'#fff',
-     marginTop: 15,
-   alignSelf:'center',
-    },
-   inputText:{
-   width:330,
-   height: 40, 
+    marginTop: 15,
+    alignSelf:'center',
+  },
+   
+inputText:{
+    width:330,
+    height: 40, 
     backgroundColor:'#fff',
     marginBottom: 6,
 	padding: 10,   
     fontSize: 20,
-     marginLeft: 10,
+    marginLeft: 10,
     marginRight:10,
-   
-	  
   },
-
 });
